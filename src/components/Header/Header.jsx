281c9df1c7bf5e3dd641dd2a3b2d0fdb9e7f0ebd
Kolya Samchuk <mykola.samchuk@gmail.com>
2023-03-06 16:17:42 +0200

    add header with responsive and mobile menu. change logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,21 @@
-import React from "react";
-import "./header.scss"
-import logo from "./img/logo2.png"
+import React, { useState } from "react";
+import "./header.scss";
+import logo from "./img/logo.png";
 
 export default function Header() {
+  // add active class for media query
+  const [active, setActive] = useState(false);
+
   return (
     <>
       <header className="header-wrapp">
         <nav className="nav-wrapp">
           <div className="logo-wrapp">
             <a href="#">
-                <img src={logo} alt="logo"/>
-                <p>Trainee 2 Support</p>
+              <img src={logo} alt="logo" />
             </a>
           </div>
-          <ul className="nav-link-wrapp">
+          <ul className={`nav-link-wrapp ${active ? "active" : ""}`}>
             <li className="link-wrapp">
               <a href="#">First Line</a>
             </li>
@@ -25,6 +27,14 @@ export default function Header() {
             </li>
           </ul>
         </nav>
+        <div
+          className={`mobile-btn ${active ? "active" : ""}`}
+          onClick={() => {
+            setActive(!active);
+          }}
+        >
+          <span></span>
+        </div>
       </header>
     </>
   );
