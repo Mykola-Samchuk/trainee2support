67f2d01953ed4ebbce447275819aed68204e65e8
Kolya Samchuk <mykola.samchuk@gmail.com>
2023-05-14 18:06:32 +0300

    add small corrected in header and general section

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,12 +3,14 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import { Outlet } from "react-router-dom";
 
-export default function Layout(){
-    return(
-        <>
-        <Header></Header>
+export default function Layout() {
+  return (
+    <>
+      <Header></Header>
+      <main>
         <Outlet></Outlet>
-        <Footer></Footer>
-        </>
-    )
+      </main>
+      <Footer></Footer>
+    </>
+  );
 }
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -104,7 +104,6 @@ export default function About() {
             </div>
           </div>
         </div>
-        <div className="spacer-xl"></div>
       </section>
     </main>
   );
