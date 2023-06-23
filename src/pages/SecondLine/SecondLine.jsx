2254eb48709f16d9c2b9d9a9b0a1d32fc8b2fb23
Kolya Samchuk <mykola.samchuk@gmail.com>
2023-06-23 13:09:42 +0300

    add progress blc for second and third pages

--- src/pages/SecondLine/SecondLine.jsx
+++ src/pages/SecondLine/SecondLine.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import "./second-line.scss";
 import Banner from "../../components/Banner/Banner";
+import img from "./img/racoon-checkbox-01.png";
 
 export default function SecondLine() {
   // background for banner
@@ -9,7 +11,19 @@ export default function SecondLine() {
   return (
     <main>
       <Banner title={"Друга лінія"} bgStyle={bgStyles.bgBlueOrange} />
-      <section className="section-content">в процесі...</section>
+      <section className="section-content section-content">
+        <div className="spacer-xl"></div>
+        <div className="content-blc-full">
+          <div className="progress-wrapp">
+            <div className="content-blc-wrapp">
+              <h2 className="content-blc-title">В процесі розробки...</h2>
+            </div>
+            <div className="img-blc">
+              <img src={img} alt="img-raccon" />
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
--- src/pages/ThirdLine/ThirdLine.jsx
+++ src/pages/ThirdLine/ThirdLine.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Banner from "../../components/Banner/Banner";
+import img from "./img/racoon-checkbox-01.png";
 
 export default function FirstLine() {
   // background for banner
@@ -9,7 +10,19 @@ export default function FirstLine() {
   return (
     <main>
       <Banner title={"Третя лінія"} bgStyle={bgStyles.bgPurpleBlue} />
-      <section className="section-content">в процесі...</section>
+      <section className="section-content section-content">
+        <div className="spacer-xl"></div>
+        <div className="content-blc-full">
+          <div className="progress-wrapp">
+            <div className="content-blc-wrapp">
+              <h2 className="content-blc-title">В процесі розробки...</h2>
+            </div>
+            <div className="img-blc">
+              <img src={img} alt="img-raccon" />
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
